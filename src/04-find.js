4ae85d4c53c620e25f58795be4574ce97a51ab3b
Brandon Bennett <107384180+codinghashira1120@users.noreply.github.com>
2022-07-31 16:18:11 -0400

    Completed Lab

--- src/04-find.js
+++ src/04-find.js
@@ -13,21 +13,30 @@ const exampleSongData = require("../data/songs");
  * @param {Object[]} songs - An array of songs. See the song data for more.
  * @returns {Object} A single object.
  */
-function findPinkElephantsByTimestreet(songs) {}
+function findPinkElephantsByTimestreet(songs) {
+  let pink = songs.find((song) => song.title === "Pink Elephants");
+  return pink
+}
 
 /**
  * Returns the first song in the list that is under three minutes.
  * @param {Object[]} songs - An array of songs. See the song data for more.
  * @returns {Object} A single object.
  */
-function findFirstSongUnderThreeMinutes(songs) {}
+function findFirstSongUnderThreeMinutes(songs) {
+  let songLength = songs.find((song) => song.runtimeInSeconds < 180)
+  return songLength
+}
 
 /**
  * Returns the first song in the list where the song title equals the song album.
  * @param {Object[]} songs - An array of songs. See the song data for more.
  * @returns {Object} A single object.
  */
-function findFirstTitleTrack(songs) {}
+function findFirstTitleTrack(songs) {
+  let titleTrack = songs.find((song) => song.title === song.album)
+  return titleTrack
+}
 
 module.exports = {
   findPinkElephantsByTimestreet,
--- src/05-some-every.js
+++ src/05-some-every.js
@@ -13,21 +13,30 @@ const exampleSongData = require("../data/songs");
  * @param {Object[]} songs - An array of songs. See the song data for more.
  * @returns {boolean}
  */
-function allSongsAreOverTwoMinutes(songs) {}
+function allSongsAreOverTwoMinutes(songs) {
+  let minutes = songs.every((song) => {song.runtimeInSeconds > 120})
+  return minutes
+}
 
 /**
  * Returns `true` if any song is over four minutes. Otherwise, return `false`.
  * @param {Object[]} songs - An array of songs. See the song data for more.
  * @returns {boolean}
  */
-function anySongIsOverFourMinutes(songs) {}
+function anySongIsOverFourMinutes(songs) {
+  let minutes = songs.some((song) => song.runtimeInSeconds > 240)
+  return minutes
+}
 
 /**
  * Returns `true` if any song is by the artist "Peanut". Otherwise, return `false`.
  * @param {Object[]} songs - An array of songs. See the song data for more.
  * @returns {boolean}
  */
-function anySongIsByPeanut(songs) {}
+function anySongIsByPeanut(songs) {
+ let peanut = songs.some((song) => song.artist === "Peanut");
+  return peanut
+}
 
 module.exports = {
   allSongsAreOverTwoMinutes,
